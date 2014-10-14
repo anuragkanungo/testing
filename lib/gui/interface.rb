85d1ed1d617607a8209a2ca4bde2a7cc7ffacf7e
anuragkanungo <anuragkanungo@gmail.com>
2014-10-14 15:02:50 +0530

    Added Button 2 implementation with spec

--- lib/gui/interface.rb
+++ lib/gui/interface.rb
@@ -12,6 +12,8 @@ class Gui::Interface
   def draw(graphics)
     button1 = Gui::Button.new(@x,@y,@width,@height,@color,"1")
     button1.draw(graphics)
+    button2 = Gui::Button.new(@x+@width,@y,@width,@height,@color,"2")
+    button2.draw(graphics)
   end
 
 end
--- spec/gui/interface_spec.rb
+++ spec/gui/interface_spec.rb
@@ -2,7 +2,7 @@ require_relative '../gui_spec_helper'
 
 describe Gui::Interface do
 
-  it "contains button with text 1" do
+  it "contains button with text 1-9" do
     graphics = double()
     interface = Gui::Interface.new(100,100,20,20,Color.black)
     expect(graphics).to receive(:setColor).with(Color.black)
@@ -11,6 +11,12 @@ describe Gui::Interface do
     expect(graphics).to receive(:draw_line).with(120,80,120,100)
     expect(graphics).to receive(:draw_line).with(120,80,100,80)
     expect(graphics).to receive(:drawString).with("1",105,95)
+    expect(graphics).to receive(:setColor).with(Color.black)
+    expect(graphics).to receive(:draw_line).with(120,100,140,100)
+    expect(graphics).to receive(:draw_line).with(120,100,120,80)
+    expect(graphics).to receive(:draw_line).with(140,80,140,100)
+    expect(graphics).to receive(:draw_line).with(140,80,120,80)
+    expect(graphics).to receive(:drawString).with("2",125,95)
     interface.draw(graphics)
   end
 
